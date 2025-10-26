28b7040fb46ac67a676777f9a7456877f1f7737b
akogarechau <akorevial@gmail.com>
2025-10-27 00:48:14 +0500

    решение второго задания первой практики

---
+++ VSC files/Програмирование (пара)/сам/практика№1/ex2.py
@@ -0,0 +1,43 @@
+"""
+ex2.py 
+
+Код для решение второго практического задания: 'Олимпиада'
+
+Основные возможности:
+- Определяет позицию из полученного балла
+- Определяет, вошел ли пользователь в топ 3
+"""
+
+from random import randint
+from heapq import nlargest
+
+# генерируем список студентов, иммитируя полученный с сайта список
+student_scores = []
+amount_of_student = 42
+repeats = 0
+for student_position in range(1,amount_of_student+1+repeats):
+    score = randint(1, 100)
+    if score not in student_scores:
+        student_scores.append(score)
+    else:
+        repeats += 1
+
+# задаем позицию пользователя, иммитируя то, что пользователь знает сколько получил баллов (позицию в списке)
+user_position = int(input('напишите вашу позицию в списке оценок: ')) 
+# предополагается что список оценок отсоритрован по фамилиям (от А до Я)
+
+def check_winners(scores: list, student_score: int):
+    """
+    Выводит ответ на вопрос - попал пользователь в топ 3 или нет
+
+    Args:
+        scores (list): список оценок студентов
+        student_score (int): оценка пользователя
+    """    
+    if student_scores[user_position - 1] in sorted(student_scores, reverse=True)[:3]:
+        print('Вы в тройке победителей!')
+    
+    else:
+        print('Вы не попали в тройку победителей.')
+
+check_winners(student_scores, user_position)
