f8ef681a9e69b57043931ff1be85e0b53bdbc56f
akogarechau <akorevial@gmail.com>
2025-10-28 14:35:23 +0500

    добавил документацию к решению 3го задания

--- VSC files/Програмирование (пара)/сам/практика№2/ex3.py
+++ VSC files/Програмирование (пара)/сам/практика№2/ex3.py
@@ -4,69 +4,144 @@ ex3.py
 Код для решения третьего практического задания
 
 Основные возможности:
-    1. Позволяет каждому транспортному средству возвращать собственное описание 
+    • Реализация онлайн-магазина:
+        1. Создайте модель для онлайн-магазина с
+            классами Product, Order, Customer, и
+            ShoppingCart.
+        2. Product включает информацию о цене, наличии
+            на складе и категории товара. Order
+            обрабатывает процесс покупки, включая расчет
+            цены с учетом скидок и налогов.
+        3. Customer управляет информацией о пользователе
+            и его истории заказов.
+        4. ShoppingCart позволяет добавлять, удалять и
+            обновлять количество товаров перед
+            оформлением заказа.
 """
 
 class Product:
     
     def __init__(self, name: str, category: str, price: int, in_stock: int):
-        
+        """
+        Инициализация продукта
+
+        Args:
+            name (str): название
+            category (str): категория
+            price (int): цена
+            in_stock (int): налицие в магазине
+        """        
         self.name = name
         self.category = category
         self.price = price
         self.in_stock = in_stock
         
 class Customer:
-    
+      
     def __init__(self, name: str, age: int):
+        """
+        Инициализация покупателя
 
+        Args:
+            name (str): имя
+            age (int): возраст
+            _orders (list): список заказов
+        """        
         self.name = name
         self.age = age
         self._orders = list()
 
+    def add_order(self, order: Order) -> None:
+        """
+        добавление заказа в историю
+
+        Args:
+            order (Order): заказ
+        """        
+        self._orders.append(order)
+        
     @property
     def orders_history(self) -> list:
-        
+        """
+        Получение истории заказов пользователя
+
+        Returns:
+            list: история заказов, их стоимость
+        """        
         return [order.total() for order in self._orders]
-    
-    def add_order(self, order: Order) -> None:
-        
-        self._orders.append(order)
 
 class Order:
     
     def __init__(self, shopping_cart: Shopping_cart, tax, discount):
-        
+        """
+        Инициализация заказа
+
+        Args:
+            shopping_cart (Shopping_cart): корзина
+            tax (_type_): налог
+            discount (_type_): скидка
+        """        
         self.shopping_cart = shopping_cart
         self.tax = tax
         self.discount = discount
 
-    def total(self):
-        
+    def total(self) -> int:
+        """
+        получение суммы заказа с учетом скидки и налога 
+
+        Returns:
+            int: сумма заказа с учетом скидки и налога
+        """        
         return round(self.shopping_cart.total() * 100 / (100 - self.tax) * (100 - self.discount) / 100, 2)
         
 class Shopping_cart:
     
     def __init__(self):
+        """
+        Инициализация корзины с товарами
         
+        Args:
+            _cart (list): корзина
+        """         
         self._cart: list[Product] = list()
-
+    
     @property    
     def cart_info(self) -> list:
-        
+        """
+        Выводит список продуктов находящихся в корзине
+
+        Returns:
+            list: список продуктов находящихся в корзине
+        """        
         return [[product.name, amount] for [product, amount] in self._cart]
     
-    def add_product(self, product: Product, amount) -> None:
-        
+    def add_product(self, product: Product, amount: int) -> None:
+        """
+        Добавлет продукт в корзину
+
+        Args:
+            product (Product): выбранный продукт
+            amount (int): кол-во выбранного продукта
+        """        
         if product.in_stock:
             self._cart.append([product, amount])
             
     def remove_product(self, product_index: Product) -> None:
-        
+        """
+        Удаление продукта из корзины
+
+        Args:
+            product_index (Product): Позиция продукта в корзине
+        """        
         self._cart.pop(product_index)
         
     def total(self) -> int:
-        
+        """
+        Получение суммы цен всех продуктов с учетом кол-ва каждого выбраного продукта
+
+        Returns:
+            int: сумма цен всех продуктов с учетом кол-ва каждого выбраного продукта
+        """        
         return sum(product.price * amount for [product, amount] in self._cart)
     
 milk = Product('Молоко 3,2%', 'Молочная продукция', 106, True)
