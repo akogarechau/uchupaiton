386bcdcc540dc8777a005eeb72b1bc3932367af4
akogarechau <akorevial@gmail.com>
2025-11-12 07:28:06 +0000

    Update autocommit.sh

--- other_tasks/autocommit.sh
+++ other_tasks/autocommit.sh
@@ -44,7 +44,7 @@ git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
 git add "$PY_FILE"
-git commit -m "Auto-commit: Task done $(date +"%Y-%d-%m || str(int(%H)+5):%M")"
+git commit -m f"Auto-commit: Task done $(date +"%Y-%d-%m || {int(%H)+5}:%M")"
 git push
 
 # ==== Очистка состояния ====
