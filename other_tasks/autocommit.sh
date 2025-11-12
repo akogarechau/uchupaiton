0c399abdab41fdc0f72dfe20fcc26d87015932e4
akogarechau <akorevial@gmail.com>
2025-11-12 13:38:50 +0000

    Fix date format in autocommit message

--- other_tasks/autocommit.sh
+++ other_tasks/autocommit.sh
@@ -44,7 +44,7 @@ git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
 git add "$PY_FILE"
-git commit -m "Sloved previous problem: Task done $(date +"%Y-%d-%m || %H:%M")"
+git commit -m "Sloved previous problem: Task done $(date +"%Y-%d-%m || %H:%M UTC")"
 git push
 
 # ==== Очистка состояния ====
