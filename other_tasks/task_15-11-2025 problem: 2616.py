4e1209afded23c21fc67c0924f4da5bebb5c1890
akogarechau <akorevial@gmail.com>
2025-11-15 16:28:15 +0000

    Update task_15-11-2025 problem: 2616.py

--- other_tasks/task_15-11-2025 problem: 2616.py
+++ other_tasks/task_15-11-2025 problem: 2616.py
@@ -3,6 +3,6 @@ def is_palindrome(s):
     s = ''.join(c for c in s.lower() if c.isalnum())
     return s == s[::-1]
 
-print("'А роза упала на лапу Азора' - палиндром?", is_palindrome("А роза упала на лапу Азора"))
+print("'А роза упала на лапу зора' - палиндром?", is_palindrome("А роза упала на лапу Азора"))
 
 # Задача
