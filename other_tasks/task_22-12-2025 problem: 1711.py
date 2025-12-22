2e98527553b4cb887872f8e4b1e64c1a6e2ee9c4
akogarechau <akorevial@gmail.com>
2025-12-22 11:17:33 +0000

    Sloved problem: Task done 2025-22-12 || 11:17 UTC

---
+++ other_tasks/task_22-12-2025 problem: 1711.py
@@ -0,0 +1,12 @@
+# 2. Проверка числа на простоту
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print("Число 17 простое?", is_prime(17))
+
+# Задача
